2f57bbd3aeaf965f851fb6565022714014d5e51c
Ivaylo Atanasov <aivo@devlabs.bg>
2018-05-11 16:18:30 +0300

    Added db seeder organelle.

---
+++ server/organelles/seeder.js
@@ -0,0 +1,30 @@
+const User = require('../models/user')
+
+module.exports = function Seeder(plasma, dna) {
+
+  this.dna = dna
+
+  if (this.dna.reactOn) {
+    plasma.on(this.dna.reactOn, module.exports.seedIdDbEmpty, this)
+  } else {
+    module.exports.seedIdDbEmpty()
+  }
+}
+
+module.exports.seedIdDbEmpty = () => {
+  User.count({}, (err, count) => {
+    if (err) return console.log(err)
+    if (count !== 0) return console.log('Seeding already done, continue...')
+
+    // create single user
+    User.create({
+      email: 'aivo@devlabs.bg',
+      password: 'nd2ibd57GJlAjOpS',
+      firstname: 'George',
+      lastname: 'Restful'
+    }, (err, user) => {
+      if (err) return console.log(err)
+      return console.log('Seed done.')
+    })
+  })
+}
